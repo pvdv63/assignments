cce6e12ccee1fafbc12d418aa2a789183f11d0bb
braadworst <roy@royniels.nl>
2016-05-26 19:39:22 +0200

    added assignments

---
+++ public/assignments/01-datatypes/braadworst/js/script.js
@@ -0,0 +1,38 @@
+// Below are the assignments for Datatypes
+
+/*
+  In the array below are a couple of odd numbers, we want to retrieve those numbers.
+  Create a new variable called 'odds' and 'push' all odd numbers to this new array using some sort of loop
+  Display the odds array to the console.
+*/
+let all = [12, 1238, 3724, 43, 89, 11, 32443287];
+
+/*
+  We want to round the decimal below, write some code that rounds the decimal to 8 and 7
+*/
+let decimal = 7.5;
+
+/*
+  In the following string replace 'and' with 'or'
+*/
+let str = "Hello and welcome to javascript!"
+
+/*
+  Remove the last element from array a
+  Remove the first element from array b
+  Combine array a and b into one array called c
+  Sort the array alphabetically
+*/
+let a = ['cookiemonster', 'grover', 'big bird', 'bert', 'ernie'];
+let b = ['kermit', 'miss piggy', 'statler', 'waldorf'];
+
+/*
+  Remove the element 'heineken' from the beers array
+*/
+let beers = ['amstel', 'heineken', 'grolsch', 'hertog jan'];
+
+/*
+  Convert the following string to an array, every word has to be a new element.
+  The last two words have to be a single element and the code cannot contain a fixed numbers so no 3 as a limit
+*/
+let toBeArray = 'I am the javascript king';
--- public/assignments/01-datatypes/braadworst/script.js
+++
@@ -1,1 +0,0 @@
-console.log('this is from the file');
